86d8faea89e145e042e453ec127b67cc15a685c3
Aniket Pathak <aaniket.7675@gmail.com>
2026-07-12 14:17:28 +0530

    FIX: css folders for global

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import "./styles/login.css"
-import "./styles/dashboard.css"
-import "./styles/students.css"
-import "./styles/fees.css"
 import "./styles/studentprofile.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
